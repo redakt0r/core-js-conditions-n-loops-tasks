0f82ac48a31f6d2f50a3ec2ca3b95a61b2be2f5c
Mikhail Koryakin <sdrr87@gmail.com>
2024-01-08 17:28:50 +0300

    feat: is contain number

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -233,8 +233,13 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = `${num}`;
+  const char = `${digit}`;
+  for (let i = 0; i < str.length; i += 1) {
+    if (char === str[i]) return true;
+  }
+  return false;
 }
 
 /**
